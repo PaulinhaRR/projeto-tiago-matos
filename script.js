6e774ebe6f1e3ae290711ccc31ffcf13fac5fdca
Paula <paulinharro@gmail.com>
2024-12-10 15:46:57 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para mostrar um alerta
+function mostrarAlerta() {
+    alert("Você clicou no botão!");
+}
+
+// Função para alterar o texto de um parágrafo
+function alterarTexto() {
+    document.getElementById("paragrafo").innerText = "O texto foi alterado com sucesso!";
+}
